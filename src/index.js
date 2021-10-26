65693b6434de40a55ac76049e886f537e0e31609
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2021-10-26 11:34:06 -0500

    Modificación del puerto

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+// app.set('port', process.env.PORT || 3000);
 
 // MIddlewares
 app.use(express.json());
+app.use(bodyParser.json());
 
 //Routes
 app.use(require('./routes/productos'));
 
-app.listen(3000, () => {
-    console.log('Server on port ' + app.get('port') + ' !!!');
+app.listen(PORT, () => {
+    console.log('Server on port ' + PORT + ' !!!');
 });
