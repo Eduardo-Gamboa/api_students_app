02430bf4ed65e60dd46e9f50ec610383d28a1955
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2021-10-26 03:52:02 -0500

    sss

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 //bc2378c6b1596d:3d449b02@us-cdbr-east-04.cleardb.com/heroku_0143efdc9011200
 const mysqlConnection = mysql.createConnection({
-    host: '@us-cdbr-east-04.cleardb.com',
+    host: 'us-cdbr-east-04.cleardb.com',
     user: 'bc2378c6b1596d',
     password: '3d449b02',
     database: 'heroku_0143efdc9011200',
