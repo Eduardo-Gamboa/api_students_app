740ec86a34cf9a8bd6a58d4b474744bc5492de3c
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2021-10-26 11:35:13 -0500

    BD modificado

--- config/db.js
+++ config/db.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 //bc2378c6b1596d:3d449b02@us-cdbr-east-04.cleardb.com/heroku_0143efdc9011200
 const mysqlConnection = mysql.createConnection({
-    // host: 'us-cdbr-east-04.cleardb.com',
-    // user: 'bc2378c6b1596d',
-    // password: '3d449b02',
-    // database: 'heroku_0143efdc9011200',
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'school',
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bc2378c6b1596d',
+    password: '3d449b02',
+    database: 'heroku_0143efdc9011200',
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'school',
     multipleStatements: true
 });
 
