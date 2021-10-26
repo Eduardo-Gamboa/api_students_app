65480713792d56489dcaa2f02e666e59d32e005b
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2021-10-26 03:46:59 -0500

    add config db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
+//bc2378c6b1596d:3d449b02@us-cdbr-east-04.cleardb.com/heroku_0143efdc9011200
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'school',
+    host: '@us-cdbr-east-04.cleardb.com',
+    user: 'bc2378c6b1596d',
+    password: '3d449b02',
+    database: 'heroku_0143efdc9011200',
     multipleStatements: true
 });
 
