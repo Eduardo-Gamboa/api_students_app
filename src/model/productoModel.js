468d03a213ba5e03afe8a6940d49c9affbb147a7
Eduardo Gamboa <69367792+Eduardo-Gamboa@users.noreply.github.com>
2021-10-28 11:32:05 -0500

    sss

--- src/model/productoModel.js
+++ src/model/productoModel.js
@@ -6,10 +6,10 @@ module.exports = {
         con.query("Select *from students where id = ?", [id], funcion);
     },
     insert: function(con, datos, funcion) {
-        con.query("insert into students(name, grade, group_, email) values (?, ?, ?, ?)", [datos.name, datos.grade, datos.group_, datos.email], funcion);
+        con.query("insert into students(name, grade, group_, email, direccion, lat, long) values (?, ?, ?, ?, ?, ?, ?)", [datos.name, datos.grade, datos.group_, datos.email, datos.direccion, datos.lat, datos.long, ], funcion);
     },
     update: function(con, datos, id, funcion) {
-        con.query("update students SET name = ?, grade = ?, group_ = ?, email = ? where id = ?", [datos.name, datos.grade, datos.group_, datos.email, id], funcion);
+        con.query("update students SET name = ?, grade = ?, group_ = ?, email = ?, direccion = ?, lat = ?, long = ? where id = ?", [datos.name, datos.grade, datos.group_, datos.email, datos.direccion, datos.lat, datos.long, id], funcion);
     },
     delete: function(con, id, funcion) {
         con.query("DELETE FROM students WHERE id = ?", [id], funcion);
